92da058841b7327388bd87108454805897acd79c
JSGrondin <js_grondin@hotmail.com>
2019-01-14 14:58:08 -0500

    -Clean & Format improved to reduce number of lines.

--- Clean_and_Format.py
+++ Clean_and_Format.py
@@ -27,5 +27,40 @@ test['Fare'] = test['Fare'].fillna(test['Fare'].median())
 train['Embarked'] = train['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
 test['Embarked'] = test['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
 
+#identify titles in Names and convert to categorical values
+train['Title'] = train.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+test['Title'] = test.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+train['Title'] = train['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+        'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+train['Title'] = train['Title'].replace('Mlle', 'Miss')
+train['Title'] = train['Title'].replace('Ms', 'Miss')
+train['Title'] = train['Title'].replace('Mme', 'Mrs')
+test['Title'] = test['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+        'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+test['Title'] = test['Title'].replace('Mlle', 'Miss')
+test['Title'] = test['Title'].replace('Ms', 'Miss')
+test['Title'] = test['Title'].replace('Mme', 'Mrs')
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+train['Title'] = train['Title'].map(title_mapping)
+train['Title'] = train['Title'].fillna(0)
+test['Title'] = test['Title'].map(title_mapping)
+test['Title'] = test['Title'].fillna(0)
+
+#droping features: Name and PassengerId
+train = train.drop(['Name', 'PassengerId'], axis=1)
+test = test.drop(['Name'], axis=1)
+
+#creating feature: FamilySize and IsAlone
+train['FamilySize'] = train['SibSp'] + train['Parch'] + 1
+test['FamilySize'] = test['SibSp'] + test['Parch'] + 1
+train['IsAlone'] = 0
+train.loc[train['FamilySize'] == 1, 'IsAlone'] = 1
+test['IsAlone'] = 0
+test.loc[train['FamilySize'] == 1, 'IsAlone'] = 1
+
+#Save cleaned files to .csv
 train.to_csv('train_cleaned.csv')
 test.to_csv('test_cleaned.csv')
